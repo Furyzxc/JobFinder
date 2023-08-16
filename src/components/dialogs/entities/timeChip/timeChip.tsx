811332465c0625e28b7103f47f667fa04f6edac4
Furyzxc <sergejananev48@gmail.com>
2023-08-16 13:43:56 +0100

    Update timeChip.tsx

--- src/components/dialogs/entities/timeChip/timeChip.tsx
+++ src/components/dialogs/entities/timeChip/timeChip.tsx
@@ -1,4 +1,4 @@
-import { Chip, Divider } from '@mui/material'
+import { Divider } from '@mui/material'
 
 type PropsType = {
 	time: string
@@ -6,8 +6,8 @@ type PropsType = {
 
 export const TimeChip = ({ time }: PropsType) => {
 	return (
-		<Divider sx={{ mb: '10px', textTransform: 'none' }}>
-			<Chip sx={{ bgcolor: '#1A1F24' }} label={time} />
+		<Divider sx={{ mb: '10px', textTransform: 'none', color: 'white' }}>
+			{time}
 		</Divider>
 	)
 }
