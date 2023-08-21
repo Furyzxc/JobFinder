412e4f732379a4314481cd113a49516dcd2c3b4d
Furyzxc <sergejananev48@gmail.com>
2023-08-21 14:41:30 +0100

    updated 404 page

--- src/components/notFound/page/notFound.tsx
+++ src/components/notFound/page/notFound.tsx
@@ -1,13 +1,15 @@
 import { useNavigate } from 'react-router-dom'
+import { useSmoothAppearance } from '@/shared/model/hooks'
 import s from './notFound.module.css'
 
 export const NotFound = () => {
 	const navigate = useNavigate()
 
+	const { ref } = useSmoothAppearance()
 	const handleGoToHmPageClick = () => navigate('/profile')
 
 	return (
-		<div className={s.box}>
+		<div className={s.box} ref={ref}>
 			<div className={s.notfound}>
 				<div className={s.notfound404}>
 					<h1>Oops!</h1>
