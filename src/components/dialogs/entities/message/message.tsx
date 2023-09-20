8c3728decf6ebd956875b535a68ea230a4787bcb
Furyzxc <sergejananev48@gmail.com>
2023-09-20 20:09:11 +0100

    animations

--- src/components/dialogs/entities/message/message.tsx
+++ src/components/dialogs/entities/message/message.tsx
@@ -2,6 +2,7 @@ import DoneAllIcon from '@mui/icons-material/DoneAll'
 import { Box, Typography } from '@mui/material'
 import { clsx } from 'clsx'
 import { memo } from 'react'
+import { WithSlide } from '@/shared/hoc'
 import { formatTime } from '@/shared/lib/format-time.ts'
 import { useCopyText } from '@/shared/model/hooks'
 import { MessageType } from '@/components/dialogs/model'
@@ -20,30 +21,32 @@ export const Message = memo(
 		const { textRef, copyText } = useCopyText()
 
 		return (
-			<Box
-				sx={{
-					bgcolor: me ? 'info.dark' : 'info.main',
-					overflow: 'hidden',
-					opacity: pending ? 0.5 : 1,
-				}}
-				className={clsx(s.message, me ? s.myMessage : s.friendMessage)}
-				onClick={copyText}
-			>
-				<div className={s.text} ref={textRef}>
-					{body}
-				</div>
-				<Typography
-					color={me ? 'warning.main' : 'warning.light'}
-					className={s.time + ' notranslate'}
+			<WithSlide direction={'up'} open={true}>
+				<Box
+					sx={{
+						bgcolor: me ? 'info.dark' : 'info.main',
+						overflow: 'hidden',
+						opacity: pending ? 0.5 : 1,
+					}}
+					className={clsx(s.message, me ? s.myMessage : s.friendMessage)}
+					onClick={copyText}
 				>
-					{time}
-				</Typography>
-				<div className={s.tick + ' notranslate'}>
-					<DoneAllIcon
-						sx={{ fontSize: '12px', color: viewed ? '#66B7F6' : 'white' }}
-					/>
-				</div>
-			</Box>
+					<div className={s.text} ref={textRef}>
+						{body}
+					</div>
+					<Typography
+						color={me ? 'warning.main' : 'warning.light'}
+						className={s.time + ' notranslate'}
+					>
+						{time}
+					</Typography>
+					<div className={s.tick + ' notranslate'}>
+						<DoneAllIcon
+							sx={{ fontSize: '12px', color: viewed ? '#66B7F6' : 'white' }}
+						/>
+					</div>
+				</Box>
+			</WithSlide>
 		)
 	}
 )
--- src/components/dialogs/features/messages/messages.tsx
+++ src/components/dialogs/features/messages/messages.tsx
@@ -1,4 +1,3 @@
-import { AnimatePresence, motion } from 'framer-motion'
 import { memo } from 'react'
 import { useParams } from 'react-router-dom'
 import { useSmoothAppearance } from '@/shared/model/hooks'
@@ -13,15 +12,9 @@ type PropsType = {
 	isLoading: boolean
 }
 
-const animations = {
-	initial: { scale: 0.5 },
-	animate: { scale: 1 },
-	exit: { opacity: 0 },
-	transition: { type: 'spring', stiffness: 65, bounce: 0.25, duration: 0.2 },
-} as const
-
 export const Messages = memo(({ isLoading }: PropsType) => {
 	const { ref } = useSmoothAppearance()
+
 	const { userId: urlId = 0 } = useParams()
 
 	const { formattedToMMMM_D, isDateExist } = useIsDateExist()
@@ -33,17 +26,15 @@ export const Messages = memo(({ isLoading }: PropsType) => {
 			{isLoading ? (
 				<MessagesSkeletons />
 			) : messages && messages.length > 0 ? (
-				<AnimatePresence>
-					{messages.map(({ id, ...message }) => (
-						<motion.div key={id} {...animations}>
-							{!isDateExist(message.addedAt) && (
-								<TimeChip time={formattedToMMMM_D(message.addedAt)} />
-							)}
-							{/*        comparing sender id with id in url, if its not equal me = true*/}
-							<Message {...message} me={message.senderId !== +urlId} />
-						</motion.div>
-					))}
-				</AnimatePresence>
+				messages.map(({ id, ...message }) => (
+					<div key={id}>
+						{!isDateExist(message.addedAt) && (
+							<TimeChip time={formattedToMMMM_D(message.addedAt)} />
+						)}
+						{/*        comparing sender id with id in url, if its not equal me = true*/}
+						<Message {...message} me={message.senderId !== +urlId} />
+					</div>
+				))
 			) : (
 				<Div>Enter your first message</Div>
 			)}
